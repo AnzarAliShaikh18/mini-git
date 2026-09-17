ff83c823b2ccea220377c067d7bc4c3c7a0196d3
AnzarAliShaikh18 <anzaralishaikh9422@gmail.com>
2026-09-17 21:16:53 +0530

    day 1
    Today i learn basic filesystem- how to create folder with name and nested folders using directories
    I also learn about argc and argv command line argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <filesystem>
+#include<fstream>
+#include <string>
 
-int main() {
-    std::cout << "Mini Git starting...\n";
+namespace fs =std::filesystem;
+void initreprository()
+{
+    if(fs::exists(".minigit"))
+    {
+        std::cout<<"Mini Git repositroy already exists";
+        return;
+    }
+
+    fs::create_directory(".minigit");
+    fs::create_directory(".minigit/objects");
+    fs::create_directories(".minigit/refs/heads");
+    std::ofstream headFile(".minigit/HEAD");
+    headFile << "ref: refs/heads/main\n";
+    headFile.close();
+    std::cout<<"Mini Git repository initialized successfully.\n";
+}
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        std::cout << "Usage: minigit <command>\n";
+        return 1;
+    }
+
+    std::string command = argv[1];
+
+    if (command == "init")
+    {
+        initreprository();
+    }
+    else
+    {
+        std::cout << "Unknown command: " << command << "\n";
+    }
+
     return 0;
 }
